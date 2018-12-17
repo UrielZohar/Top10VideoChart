16d27f3fe9f800d41b7db51fd5d2460f1c59746c
Uriel Zohar <urielfed@gmail.com>
2018-12-17 13:43:52 +0200

    path for index html

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.status(200).sendFile(path.join(__dirname + '/dist/top10-video-chart/index.html'));
+  res.status(200).sendFile(path.join(__dirname + '/dist/natuelIntelligence/index.html'));
 });
 
 module.exports = router;
