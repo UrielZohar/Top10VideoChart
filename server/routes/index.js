a3e0f81bc944f371c96985d84e98014aaece1cc8
Uriel Zohar <urielfed@gmail.com>
2018-12-17 14:50:46 +0200

    default path folder

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,7 @@ const logger = require('heroku-logger');
 router.get('/', function(req, res, next) {
   logger.info('Get default path get request');
   logger.info('The __dirname is: ' + __dirname);
-  res.status(200).sendFile(path.join(__dirname + '/dist/natuelIntelligence/index.html'));
+  res.status(200).sendFile(path.join(__dirname + '/../../dist/natuelIntelligence/index.html'));
 });
 
 module.exports = router;
