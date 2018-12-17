fe837c40e784a8d52052d7ce5fe143d4e13f876a
Uriel Zohar <urielfed@gmail.com>
2018-12-17 15:39:44 +0200

    correct path 1

--- server/app.js
+++ server/app.js
@@ -36,7 +36,7 @@ console.log('Definning the default API');
 app.get('/', function(req, res, next) {
   console.log('Get default path get request');
   console.log('The __dirname is: ' + __dirname);
-  res.status(200).sendFile(path.join(__dirname + '/../../dist/natuelIntelligence/index.html'));
+  res.status(200).sendFile(path.join(__dirname + '/../dist/natuelIntelligence/index.html'));
 });
 
 app.use('/api', index);
