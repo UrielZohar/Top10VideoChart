54af995f2b48dadfa7cad0b16fdb7ae6b069e2f8
Uriel Zohar <urielfed@gmail.com>
2018-12-17 14:59:32 +0200

    console log node

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const logger = require('heroku-logger');
 
 router.get('/', function(req, res, next) {
-  logger.info('Get default path get request');
-  logger.info('The __dirname is: ' + __dirname);
+  console.log('Get default path get request');
+  console.log('The __dirname is: ' + __dirname);
   res.status(200).sendFile(path.join(__dirname + '/../../dist/natuelIntelligence/index.html'));
 });
 
