c279ce353c819db9b1075fe11b8aeaff0c2cd5ff
Uriel Zohar <urielfed@gmail.com>
2018-12-17 15:06:23 +0200

    app route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const app = express();
+console.log('Definning the routes...');
 
+app.use(express.static(__dirname + '/../../dist/natuelIntelligence/index.html'));
+
+/*
 router.get('/', function(req, res, next) {
   console.log('Get default path get request');
   console.log('The __dirname is: ' + __dirname);
   res.status(200).sendFile(path.join(__dirname + '/../../dist/natuelIntelligence/index.html'));
 });
+*/
+app.get('*', function(req, res, next) {
+  console.log('Get default path get request');
+  console.log('The __dirname is: ' + __dirname);
+  res.status(200).sendFile(path.join(__dirname + '/../../dist/natuelIntelligence/index.html'));
+});
+
 
 module.exports = router;
