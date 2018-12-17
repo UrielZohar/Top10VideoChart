a03e3e8b9d9c8256b390c0f9106c299abc2c8ec7
Uriel Zohar <urielfed@gmail.com>
2018-12-17 14:39:25 +0200

    heroku logger

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const logger = require('heroku-logger');
 
 router.get('/', function(req, res, next) {
+  logger.info('Get default path get request');
+  logger.info('The __dirname is: ' + __dirname);
   res.status(200).sendFile(path.join(__dirname + '/dist/natuelIntelligence/index.html'));
 });
 
